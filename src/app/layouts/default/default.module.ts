4fa6885c53a6c57e1fde5987fcf100efd66f752f
Ashish Ahuja <ashish.ahuja@locusnine.com>
2020-04-27 20:56:50 +0530

    New Dashboard

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent    
+
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule
+  ]
+})
+export class DefaultModule { }
