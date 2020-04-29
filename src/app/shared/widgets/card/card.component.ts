e4b3d9643f866b36e5502480642314689e1f3f3c
Ashish Ahuja <ashish.ahuja@locusnine.com>
2020-04-29 17:01:07 +0530

    Cards working

--- src/app/shared/widgets/card/card.component.ts
+++ src/app/shared/widgets/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import HC_exporting from 'highcharts/modules/exporting';
 
@@ -9,12 +9,78 @@ import HC_exporting from 'highcharts/modules/exporting';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input() label: string;
+  @Input() total: string;
+  @Input() percentage: string;
+  @Input() data = [];
+
   Highcharts = Highcharts;
+  chartOptions = {}
   constructor() { }
 
-  ngOnInit(){
-    HC_exporting(this.Highcharts)
+  ngOnInit() {
+    this.chartOptions = {
+
+      chart: {
+        type: 'area',
+        backgroundColor: null,
+        margin: [2,2,2,2],
+        height: 60
+      },
+      title: {
+        text: null
+      },
+      subtitle: {
+        text: null
+      },
+      tooltip: {
+        split: true,
+        outside: true
+      },
+      legend: {
+        enabled: false
+      },
+      credits: {
+        enabled: false
+      },
+      exporting: {
+        enabled: false,
+      },
+      xAxis: {
+        labels:{
+          enabled: false
+        },
+        title: {
+          text: null
+        },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: [] 
+      },
+      yAxis: {
+        labels:{
+          enabled: false
+        },
+        title: {
+          text: null
+        },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: [] 
+      },
+      series: [{
+        data: [71,78,39,66]
+      }]
+    };
+    HC_exporting(Highcharts);  //can export data with this function
+
+    setTimeout(() => {
+      window.dispatchEvent(
+        new Event('resize')
+      );
+    }, 300);
+
   }
-
-
 }
+
+
